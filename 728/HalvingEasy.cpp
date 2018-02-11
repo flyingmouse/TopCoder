015f8d80e5d8df2cf84e23136e30167f8af656af
flyingmouse <haoming.exe@gmail.com>
2018-02-11 16:12:03 +0100

    HalvingEasy

---
+++ 728/HalvingEasy.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <cassert>
+using namespace std;
+
+#define REPP(i,a,b) for(int i=(a); i < (b); ++i)
+#define REP(i,a) for(int i = 0; i < (a); ++i)
+#define PER(i,a) for(int i = (a) - 1; i >= 0; --i)
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define MP make_pair
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+class HalvingEasy {
+    public:
+    int count(vector <int> S, int T) {
+        int n = S.size(), ans = 0;
+        REP(i, n) {
+            while (S[i]) {
+                if (S[i]== T) {
+                    ans++;
+                    break;
+                }
+                S[i] >>= 1;
+            }
+        }
+        return ans;
+    }
+};
